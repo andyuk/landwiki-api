746c01134a1964fd703cd91df57ae1a233131cee
Mairead Buchan <mdb.buchan@gmail.com>
2013-11-16 11:11:56 +0000

    adding new API routes

--- routes/sites.js
+++ routes/sites.js
@@ -13,15 +13,75 @@ function toKeyValuePairs(inputObject) {
   return outputArray;
 };
 
+exports.showAll = function(req, res){
+//todo: how to return whole collection?
+  //code to get all records from collection
+  // Site.find(function(err, doc) {
+  //   if (err)
+  //     return res.send(404);
+  //   res.json(doc);
+  // });
+};
+
 exports.detail = function(req, res){
 
   var id = req.param('id');
   if (! id)
     return res.send(404);
-
   Site.findById(id, function(err, doc) {
     if (err)
       return res.send(404);
     res.json(doc);
   });
 };
+
+exports.addSite = function(req, res) {
+  
+  var site = req.body;
+  console.log('Adding site: ' + JSON.stringify(site));
+  db.collection('sites', function(err, collection) {
+    collection.insert(site, {safe:true}, function(err, result) {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        console.log('Success: ' + JSON.stringify(result[0]));
+        res.send(result[0]);
+      }
+    });
+  });
+}
+
+exports.updateSite = function(req, res){
+  var id = req.params.id;
+  var site = req.body;
+  console.log('Updating site: ' + id);
+  console.log(JSON.stringify(site));
+  db.collection('sites', function(err, collection) {
+    collection.update({'_id':new BSON.ObjectID(id)}, site, {safe:true}, function(err, result) {
+      if (err) {
+        console.log('Error updating site: ' + err);
+        res.send({'error':'An error has occurred'});
+      } else {
+        console.log('' + result + ' document(s) updated');
+        res.send(site);
+      }
+    });
+  });
+    
+}
+
+ 
+exports.deleteSite = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting site: ' + id);
+    db.collection('sites', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
